08d6015ea870a8b6ceaf516dae0d1fe5c7b04055
Kripa Sindhu <sindhukripa007@gmail.com>
2024-06-24 16:10:18 +0530

    respond with body

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    if (request.startsWith("GET / ")) {
+    if (request.startsWith("GET /echo ")) {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
