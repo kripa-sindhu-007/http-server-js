7b3385621d24ad0e6c1c64996f624f033883e24d
Kripa Sindhu <sindhukripa007@gmail.com>
2024-06-24 15:55:11 +0530

    extract URL path

--- app/main.js
+++ app/main.js
@@ -5,13 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  if(request.startsWith('GET / ')){
+  if(request.startsWith('GET/')){
     socket.write("HTTP/1.1 200 OK\r\n\r\n");
   }
   else{
     socket.write("HTTP/1.1 404 NOT FOUND\r\n\r\n");
   }
-    
   socket.on("close", () => {
     socket.end();
   });
