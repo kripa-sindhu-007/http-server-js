4c0eea26e9c1491474ce6862b9aa3153ff12d92f
Kripa Sindhu <sindhukripa007@gmail.com>
2024-06-25 08:01:15 +0530

    respond with body

--- app/main.js
+++ app/main.js
@@ -3,18 +3,17 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
-    const url=request.split(' ')[1];
+    const url = request.split(" ")[1];
     if (url == "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    }
-    else if(url.includes("/echo/")){
+    } else if (url.includes("/echo/")) {
       const content = url.split("/echo/")[1];
       const actualLength = Buffer.byteLength(content, "utf8");
-      socket.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: actualLength\r\n\r\n${content}");
+      const headers = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${actualLength}\r\n\r\n`;
+      socket.write(headers + content);
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
