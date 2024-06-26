6ead914a411ed8e1d840eb34a97cd1c8a43db9c1
Kripa Sindhu <sindhukripa007@gmail.com>
2024-06-26 11:32:44 +0530

    read request body

--- app/main.js
+++ app/main.js
@@ -9,6 +9,7 @@ const server = net.createServer((socket) => {
     const request = data.toString();
     const url = request.split(" ")[1];
     const header = request.split("\r\n");
+    const method=request.split[0];
     if (url == "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (url.includes("/echo/")) {
