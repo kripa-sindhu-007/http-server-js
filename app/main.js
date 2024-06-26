6c7dcde79eba867a19e8f06244717b0167335cc2
Kripa Sindhu <sindhukripa007@gmail.com>
2024-06-26 11:03:35 +0530

    return a file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -7,7 +8,7 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const url = request.split(" ")[1];
-    const header=request.split('\r\n');
+    const header = request.split("\r\n");
     if (url == "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (url.includes("/echo/")) {
@@ -15,16 +16,24 @@ const server = net.createServer((socket) => {
       const actualLength = Buffer.byteLength(content, "utf8");
       const temp = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${actualLength}\r\n\r\n`;
       socket.write(temp + content);
-    } 
-    else if(url.includes("/user-agent")){
-      const userAgent=header[2].split('User-Agent: ')[1];
+    } else if (url.startsWith("/files/")) {
+      const dir = process.argv[3];
+      const filename = url.split("/files/")[1];
+      if (fs.existsSync("${directory}/${filename}")) {
+        const content = fs.readFileSync("${directory}/${filename}").toString();
+        const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`;
+        socket.write(res);
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
+    } else if (url.includes("/user-agent")) {
+      const userAgent = header[2].split("User-Agent: ")[1];
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
-    }
-    else {
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+      );
+    } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
-
   });
 
   socket.on("close", () => {
